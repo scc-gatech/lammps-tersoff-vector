4960b3305fe2c8a420755b1bd045d163b9d78059
petroseskinder <pj.eskinder@gatech.edu>
2017-10-15 15:53:56 -0400

    Create build script for power8

---
+++ machines/ibm-power8/build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+set -o verbose
+module load compiler/gcc-5.3.0
+
+SRC=$PWD
+
+cp -r $SRC/lammps-10Mar16 . 
+
+# copy modified files into lammps folder
+cp -r $SRC/../../src/* $SRC/lammps-10Mar16/src/
+
+cd $SRC/lammps-10Mar16/src/
+make yes-USER-OMP
+make yes-USER-INTEL
+make no-KOKKOS
+make no-GPU
+
+# remove existing power8 binary
+rm -rf lmp_power8
+
+# build power8 binary
+make -j10 power8 
+cp $SRC/lammps-10Mar16/src/lmp_power8 $SRC/
+
+
