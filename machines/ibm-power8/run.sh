df6f86a679d7d62d217d002bd88c261cde22466e
petroseskinder <pj.eskinder@gatech.edu>
2017-10-16 06:55:14 -0400

    Modified run script so Tersoff runs on power8. Needed to set KOKKOS to vanilla.

--- machines/ibm-power8/run.sh
+++ machines/ibm-power8/run.sh
@@ -2,5 +2,22 @@
 
 module purge
 module load compiler/gcc-5.3.0
-./build.sh
-./lmp_power8 -in ../../benchmarks/lammps/in.tersoff -pk omp 1 -pk balance 0 mode single
+
+# create blank file to indicate no KOKKOS
+touch pak.vanilla
+
+# Check if Tersoff parameters are passed
+FILE=Si.tersoff
+if [ -f $FILE ]; then
+   echo "File $FILE exists."
+else 
+   touch Si.tersoff
+   echo "$FILE does not exist. Creating default params"
+   default_params="
+   Si  Si   Si  3.0 1.0 1.3258 4.8381 2.0417 0.0000 22.956
+                0.33675  1.3258  95.373  3.0  0.2  3.2394  3264.7
+   "
+   echo "$default_params" > Si.tersoff
+fi
+
+./lmp_power8 -in in.tersoff -v p vanilla -sf intel -pk omp 0 -pk intel 1 balance 0 mode double
