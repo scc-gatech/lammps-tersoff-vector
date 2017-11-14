66936873e3776e9313d859f9e706dc1c0a7a343f
petroseskinder <pj.eskinder@gatech.edu>
2017-11-14 16:43:59 -0500

    accuracy results

---
+++ machines/ibm-power8/results/accuracy/accuracy-double.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+module purge 
+module load compiler/gcc-5.3.0
+module load hpcx
+
+export OMP_NUM_THREADS=4
+# basic run
+mpirun -np 20 ./lmp_power8 -in in.tersoff-acc -pk intel 1 mode double -sf intel -v p vanilla | tee acc-single > accuracy_double_results
+#./lmp_power8 -in in.tersoff -pk omp 0 -pk intel 1 mode single -sf intel -v p vanilla
+
